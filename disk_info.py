7135141c54b785eded1d54ac7418fc5880b69a16
Pascal Anene <pascal.o.anene@gmail.com>
2022-08-01 13:08:32 +0200

    Initial commit for Playground Details

---
+++ disk_info.py
@@ -0,0 +1,10 @@
+import shutil
+
+def disk_percent_usage(directory):
+        '''
+        Return perecent usage of disk where directory resides
+        '''
+        disk_info = shutil.disk_usage(directory)
+        return round(disk_info.used * 100 / disk_info.total, 2)
+
+        
